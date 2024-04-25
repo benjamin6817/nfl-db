b3c3c97ad98b64ea13b99190265dec144d152e38
Ben Easterling <beneasterling25@gmail.com>
2024-04-24 21:16:30 -0500

    connect to database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 /**
  * Import function triggers from their respective submodules:
  *
@@ -9,11 +10,31 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import mysql from "mysql2/promise";
+import {Connector, IpAddressTypes} from "@google-cloud/cloud-sql-connector";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+async function connect() {
+  const connector = new Connector();
+  const clientOpts = await connector.getOptions({
+    instanceConnectionName: "nfl-db-83d98:us-central1:nfl-db-mysql-instance",
+    ipType: IpAddressTypes.PUBLIC,
+  });
+  const pool = await mysql.createPool({
+    ...clientOpts,
+    user: "nfl-api",
+    password: "1234",
+    database: "NFL",
+  });
+  const conn = await pool.getConnection();
+  const [result] = await conn.query("SELECT NOW();");
+  console.table(result); // prints returned time value from server
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  await pool.end();
+  connector.close();
+}
+
+export const helloWorld = onRequest(async (request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  await connect();
+  response.send("Hello from Firebase!");
+});
