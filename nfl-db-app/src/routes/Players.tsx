b7cddc3481ded582fbf6d16e390b9030fdcabd57
Ben Easterling <beneasterling25@gmail.com>
2024-04-25 03:41:18 -0500

    fetch player data into UI

--- nfl-db-app/src/routes/Players.tsx
+++ nfl-db-app/src/routes/Players.tsx
@@ -1,13 +1,25 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { getPlayersOnTeam } from '../common/api';
 
 export default function Players() {
+  const [players, setPlayers] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
       const response = await getPlayersOnTeam({ teamId: 18 });
-      console.log(response);
+      setPlayers(response.data);
     }
     fetchData();
-  });
+  }, []);
 
-  return <p>Players</p>;
+  return (
+    <div className="player-list">
+      {players.map(player => (
+        <div className="player-card">
+          <p className="player-name">{player.PlayerName}</p>
+          <p className="player-position">Position: {player.Position}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
